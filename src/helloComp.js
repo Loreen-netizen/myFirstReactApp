2dd9d128fa22d5122f0a2a5ce9c4b261de614c07
Loreen-netizen <loreenmuusha@gmail.com>
2021-03-02 12:46:22 +0200

    dynamic skills element create

--- src/helloComp.js
+++ src/helloComp.js
@@ -1,28 +1,30 @@
 
-const MyInfo = (props) =>{
-return <h4>I'm {props.info}</h4>
-}
+// const MyInfo = (props) =>{
+// return <h4>I'm {props.info}</h4>
+// }
 
+
+const MySkills = () =>{
 const skillsArray = ["HTML, CSS , Bootstrap CSS","JavaScript and NodeJS","Database (PostgreSQL)","Version Control using Git And GitHub","Critical and analytical And Research and development skills.","Independent thinking and problem-solving"
-,"Communication and presentation skills","Teamwork, Adaptability/ Flexibility"]
+,"Communication and presentation skills","Teamwork, Adaptability/ Flexibility"];
 
-const MySkills = (skillsArray) =>{
-  skillsArray.map((value)=>{
-    return <ul className="list">{value}</ul>
+    return skillsArray.map(value => {
+    return <ul className="list">{value}</ul> 
+  });  
+  }
 
-  })
- }
+  
+
 
 const HelloComponant = () =>{
   return  (<div className = "bodyCss"> 
    
-  <h3 className="whiteText"> Hello, <MyInfo className = "pinkText" info="Yeukai Loreen Muusha"></MyInfo> </h3>
-  <h3 className= "pinkText"><MyInfo info = "an Agile Web Developer"/></h3>
+  <h3 className = "pinkText" > Hello,  I'm Yeukai Loreen Muusha </h3>
+  <h3 className= "pinkText">I'm an Agile Web Developer"</h3>
 
 
-<p className="paragraph"> <MyInfo info = "looking to achieve my career goal, which is to gain a role which allows me to further my 
-knowledge and skills, broadening my horizon.I enjoy the challenge of problem solving, debugging and
-creating something from nothing."/> </p>
+<p className="paragraph"> My career goal is to gain a role which allows me to further my knowledge and skills,<br/>
+ broadening my horizon.I enjoy the challenge of problem solving,<br/> debugging and creating something from nothing. </p>
 
 
 <h4 className="pinkText">I have the following skills</h4>
